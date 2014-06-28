6e85e0a7641409b82fc1dd11e5a8420761a4fdd0
Eric Finster <ericfinster@gmail.com>
2014-06-28 19:27:34 +0200

    Some more sketching of the syntax ...

--- orchard-lib/src/main/scala/orchard/core/expression/Syntax.scala
+++ orchard-lib/src/main/scala/orchard/core/expression/Syntax.scala
@@ -11,30 +11,41 @@ import orchard.core.cell._
 
 sealed trait Statement
 case class ModuleDefinition(name : String, body : List[Statement]) extends Statement
-case class VariableDefinition(name : String, shell : Shell) extends Statement
-case class LiftDefinition(name : String, nook : Nook) extends Statement
-case class Import(moduleName : String) extends Statement
+case class Parameter(ident : Identifier, varExpr : Variable) extends Statement
+case class Definition(ident : Identifier, liftExpr : Lift) extends Statement
+case class Import(module : String) extends Statement
 
-// sealed trait Expression
-
-// The idea is that nooks and shells will now have identifiers in their shells.  Or
-// perhaps rather "expressions" which are entities which evaluate to the kinds of
-// expressions you had before.
-
-// Now the question arises about the rewriting of names.  So like, how should this work?
-// I think it's like before, where the identifiers contain environment references.
-
-// Or is there some kind of difference between giving a unique identifier to a lift or
-// an expression, and rather having it depend on some kind of parameters.  This is the
-// problem of how you want to treat abstraction.
-
-// Right. I definitely think and expression should be either an identifier, an application
-// of a substitution to an identifier, or an address thereof.  Ah, or an unfolding ...
-
-sealed trait SyntaxExpression
-case class IdentExpression(id : Identifier)
+sealed trait Expression
+case class Reference(ref : String) extends Expression
+case class Substitution(shell : Shell, expr : Expression, bindings : Map[Int, Expression]) extends Expression
+case class DefinitionOf(expr : Expression) extends Expression
+case class Face(expr : Expression, faceAddress : CellAddress) extends Expression
+case class Lift(nook : Nook) extends Expression
+case class Variable(index : Int, shell : Shell) extends Expression
+case class Let(ident : Identifier, value : Expression, expr : Expression)
 
 sealed trait Identifier
 
-class Nook
-class Shell
+class Nook(ncell : NCell[Option[Expression]])
+class Shell(ncell : NCell[Option[Expression]])
+
+// The point of the above is that it is essentially a languge for describing opetopic definitions.  Neat!
+// Now what you have to do is to write a compiler/type-checker which checks whether a give collection of
+// statements makes sense.  And then write the user-interface (which should remain completely separate)
+// which allows users to build well-formed expressions with ease (since this syntax is for the most part
+// completely unusable!!)
+
+// And there are some *great* ideas to go with this!  Like consider the following: it should be trivial
+// to write this to xml.  But then you can do the following neat trick: use stylesheets or translations
+// or whatever to *render the code as html*, meaning with svg's and everything so that instead of the
+// horrible syntax which is going to define shells and ncells and whatnot, you simply have an svg image
+// generated.  So the raw code editor becomes your browser!  This will take care of rendering the pretty
+// pictures and everything.  Oh that's fucking brilliant!
+
+// Of course this is just more evidence that you need to write a javascript version of the rendering algorithm
+// so that the whole thing can be done in the browser.
+
+// Okay, but this will just have to be later.  For now you're just going to have to use the javafx ui to
+// test out if this is even plausible or not.  Then you can add the fancy features like that ....
+
+
