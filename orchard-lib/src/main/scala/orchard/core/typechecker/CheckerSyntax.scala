e78de350adf4716dda9297ba9f4d9423ae862fec
Eric Finster <ericfinster@gmail.com>
2014-08-04 20:36:47 +0200

    Resurrecting ...

--- orchard-lib/src/main/scala/orchard/core/typechecker/CheckerSyntax.scala
+++ orchard-lib/src/main/scala/orchard/core/typechecker/CheckerSyntax.scala
@@ -23,3 +23,45 @@ trait CheckerSyntax { thisChecker : Checker =>
   // in which the statements can be interpreted.
 
 }
+
+// import scalaz._
+
+// sealed trait Statement[+A]
+// case class ParameterDec[+A](name : String, next : A) extends Statement[A]
+// case class LiftDec[+A](name : String, next : A) extends Statement[A]
+// case object Done extends Statement[Nothing]
+
+// object Statement {
+
+//   implicit def isFunctor : Functor[Statement] = 
+//     new Functor[Statement] {
+
+//       def map[A, B](fa : Statement[A])(f : A => B) : Statement[B] = 
+//         fa match {
+//           case ParameterDec(name, next) => ParameterDec(name, f(next))
+//           case LiftDec(name, next) => LiftDec(name, f(next))
+//           case Done => Done
+//         }
+
+//     }
+
+//   // Now, I want to use the free monad.
+
+//   type FreeM[A] = Free[Statement, A]
+//   // val M = Monad[FreeM]
+//   import Free._
+
+//   def paramDec(name : String) : FreeM[Unit] = 
+//     Suspend(ParameterDec(name, Return(())))
+
+//   def liftDec(name : String) : FreeM[Unit] = 
+//     Suspend(LiftDec(name, Return(())))
+
+//   def test : FreeM[Unit] = 
+//     for {
+//       _ <- paramDec("Eric")
+//       _ <- liftDec("Fred")
+//     } yield ()
+
+// }
+
